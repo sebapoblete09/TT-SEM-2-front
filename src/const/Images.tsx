974fdb5c1a4cadc3a58483ed08bf405a79d01fbc
Sebastian Poblete <pseba9006@gmail.com>
2025-10-26 22:57:58 -0300

    se agrego un carrusel al hero

---
+++ src/const/Images.tsx
@@ -0,0 +1,7 @@
+const images = [
+  "/images/Workshop.webp", // Imagen de marcador de posición 1
+  "/images/Biomateriales.webp", // Imagen de marcador de posición 2
+  "/images/Innova.webp", // Imagen de marcador de posición 3
+];
+
+export default images;
